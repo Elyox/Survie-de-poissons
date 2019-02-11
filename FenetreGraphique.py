bb080111b05717dec8973263c65ad00238472297
Paulin.Marco <marco.paulin@hotmail.fr>
2019-02-11 13:03:35 +0100

    Placement nombre poisson

--- FenetreGraphique.py
+++ FenetreGraphique.py
@@ -19,8 +19,8 @@ Color=[Blanc, Bleu]
 global ListePoisson
 ListePoisson=[] #Liste des attibuts des poissons
 
-
-
+#création liste des nombres
+NombreListe=[i for i in range(2,102)]
 
 pygame.init() # initialisation fenetre
 
@@ -54,7 +54,6 @@ class Monde:
     #Résultat: Creation de la grille de deplacement des poissons
     def CréationMonde(self):
         x,y=0,0
-
         #Création des case de la grille
         for j in range (self.Largeur):
             for i in range(self.longueur):
@@ -65,26 +64,31 @@ class Monde:
                     y+=EcartCase
 
         #Placement des poissons sur la grille
-        for loop in range (self.NbsPoisson):
+        pygame.draw.rect(Display, Color[1],((ListePoisson[x].x) + 2, (ListePoisson[x].y) + 2, tailleCase - 4, tailleCase - 4))
+        for loop in range (self.NbsPoisson-1):
             pygame.draw.rect(Display,Color[1],((ListePoisson[x].x)+2,(ListePoisson[x].y)+2,tailleCase-4,tailleCase-4))
             text = font.render(str(ListePoisson[x].nombre ),0,(0,0,0))
-            Display.blit(text,((ListePoisson[x].x+(tailleCase/2))-10,(ListePoisson[x].y+(tailleCase/2))-10))
+            Display.blit(text,((ListePoisson[x].x+(tailleCase/2))-15,(ListePoisson[x].y+(tailleCase/2))-15))
             x+=1
 
 
     #Resulta: Creation des poisson au depart
     def CréationPoisson(self):
-        x,y = 0,0
-        n=2
-        ListePoisson.append(Poisson((x),(y),n))
-        for loop in range(self.NbsPoisson-1):
-            nbs=0
-            ListePoisson.append(Poisson((x),(y),n))
+        x, y = 0, 0
+        n = 0
+
+
+        for loop in range(self.NbsPoisson - 1):
+            n=random.choice(NombreListe)
+            ListePoisson.append(Poisson((x), (y), n))
+            print(NombreListe)
+            NombreListe.remove(n)
+            if len(NombreListe)==1:
+                NombreListe.append(NombreListe[0])
             x += EcartCase
             if x == (10 * EcartCase):
                 x = 0
                 y += EcartCase
-
 
                     
 
@@ -94,11 +98,14 @@ Terrain = Monde(10,10 ,102)
 #placement des poissons dans la grille
 Terrain.CréationPoisson()
 
+pygame.mouse.set_cursor(*pygame.cursors.broken_x)
+
 salut=True
 while salut:
     Display.fill((0, 0, 0))
     Terrain.CréationMonde()
-    for event in pygame.event.get(): # pour chaque evenement donner
+
+    for event in pygame.event.get(): # pour chaque évenement donner
         if event.type == pygame.QUIT:
             salut= False
     pygame.display.update()
